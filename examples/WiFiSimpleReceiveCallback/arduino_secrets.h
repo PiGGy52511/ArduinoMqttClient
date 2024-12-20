1d6cb6d45a16a3571a09df7c0a59dd4b293d8406
PiGGy52511 <32626479+PiGGy52511@users.noreply.github.com>
2024-12-20 20:55:12 +0800

    更新针脚定义

--- examples/WiFiSimpleReceiveCallback/arduino_secrets.h
+++ examples/WiFiSimpleReceiveCallback/arduino_secrets.h
@@ -1,2 +1,17 @@
 #define SECRET_SSID ""
 #define SECRET_PASS ""
+
+///故障
+#define LED_R 6
+///冲洗
+#define LED_G 10
+///制水
+#define LED_B 7
+
+#define BUTTON 11
+
+#define VALVE1_R 9
+#define VALVE1_B 8
+
+#define VALVE2_R 4
+#define VALVE2_B 5
